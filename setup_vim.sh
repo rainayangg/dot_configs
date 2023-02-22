3d81d2ad589ca388d1903a9fd95fde828b141869
rainayangg <raina.yang.chn@gmail.com>
2023-02-22 21:15:28 +0100

    Fix vim setup bug

--- setup_vim.sh
+++ setup_vim.sh
@@ -6,9 +6,6 @@ cp $(pwd)/.vimrc ~/.vimrc
 
 mv ~/.vim ~/.vim_backup
 cp -r $(pwd)/.vim ~/.vim
-curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
-    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-
 vim +PluginInstall +qall
 
 # set up NeoVim config
