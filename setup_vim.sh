a5a1e0bbeffc229415fd0a44cebc4340708a4556
rainayangg <raina.yang.chn@gmail.com>
2023-03-30 16:44:59 +0000

    fix vanilla installment missings

--- setup_vim.sh
+++ setup_vim.sh
@@ -8,8 +8,17 @@ mv ~/.vim ~/.vim_backup
 cp -r $(pwd)/.vim ~/.vim
 vim +PluginInstall +qall
 
+# set up Neovim prerequirements
+sudo apt update
+sudo apt install -y nodejs
+sudo apt install -y npm
+sudo npm i -g pyright
+sudo apt install ccls
+
 # set up NeoVim config
 mkdir -p ~/.config/nvim/
 mv ~/.config/nvim ~/.config/nvim_backup
 cp -r $(pwd)/nvim ~/.config/nvim
-nvim +PluginInstall +qall
+git clone --depth 1 https://github.com/wbthomason/packer.nvim\
+      ~/.local/share/nvim/site/pack/packer/start/packer.nvim
+nvim +PackerInstall +qall
--- setup_zsh.sh
+++ setup_zsh.sh
@@ -1,5 +1,18 @@
 #!/bin/bash
+sudo apt install zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-
 cp ./.zshrc ~/.zshrc
 
+git clone https://github.com/zsh-users/zsh-autosuggestions ~/.oh-my-zsh/custom/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
+source ./.zshrc
+
+# if chsh failed:
+# cat <<EOT >> ~/.bashrc
+# if [ "$SHELL" != "/usr/bin/zsh" ]
+# then
+#  export SHELL="/usr/bin/zsh"
+#       exec /usr/bin/zsh
+# fi
+# EOT
+# source ~/.bashrc
