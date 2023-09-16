19741c50c3283fdce77183ed65f8ca0acec6cdd5
rainayangg <raina.yang.chn@gmail.com>
2023-09-16 13:03:02 +0000

    Update nvim for iccluster setup

--- nvim/lua/rainayangg/plugins.lua
+++ nvim/lua/rainayangg/plugins.lua
@@ -47,4 +47,5 @@ return require('packer').startup(function(use)
     use 'saadparwaiz1/cmp_luasnip'
     use 'preservim/nerdcommenter'
     use 'lervag/vimtex'
+    use 'nvim-tree/nvim-web-devicons'
 end)
--- setup_vim.sh
+++ setup_vim.sh
@@ -14,11 +14,25 @@ sudo apt install -y nodejs
 sudo apt install -y npm
 sudo apt install -y xdg-utils
 sudo apt install -y ripgrep
+sudo apt install -y snap
+if type snap > /dev/null 2>&1; then
+    sudo snap refresh snapd
+    sudo snap install nvim --classic
+else
+    echo "Cannot install snap!"
+    exit 1
 
 sudo npm i -g pyright
 sudo apt install ccls
 
 # set up NeoVim config
+user_home=$(echo $HOME)
+echo "Home directory is '$user_home'"
+if [ "$user_home" = "/root" ]; then
+    export PATH="/root/.config/lsp/lua_language_server/bin/lua-language-server:$PATH" >> ~/.zshrc
+    export XDG_CONFIG_HOME="/root/.config" >> ~/.zshrc
+fi
+
 mkdir -p ~/.config/nvim/
 mv ~/.config/nvim ~/.config/nvim_backup
 cp -r $(pwd)/nvim ~/.config/nvim
