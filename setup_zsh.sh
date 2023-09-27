182e20aec8afe0879e1d6c1c48e8a21a3bc046df
rainayangg <raina.yang.chn@gmail.com>
2023-09-27 11:53:18 +0000

    Add plugin install for vi-mode

--- setup_zsh.sh
+++ setup_zsh.sh
@@ -3,6 +3,8 @@ sudo apt install -y zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 cp ./.zshrc ~/.zshrc
 
+git clone https://github.com/jeffreytse/zsh-vi-mode \
+  $ZSH/plugins/zsh-vi-mode
 git clone https://github.com/zsh-users/zsh-autosuggestions ~/.oh-my-zsh/custom/plugins/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
 source ./.zshrc
