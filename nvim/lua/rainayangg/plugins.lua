8f5dc235bf0743760abee97c21e37baa5d1da23c
Yang Rui <ryang@iccluster045.iccluster.epfl.ch>
2024-03-19 19:23:56 +0100

    fix old bug in zsh script and nvimtree

--- nvim/lua/rainayangg/plugins.lua
+++ nvim/lua/rainayangg/plugins.lua
@@ -44,7 +44,7 @@ return require('packer').startup(function(use)
         requires = {
             'nvim-tree/nvim-web-devicons', -- optional, for file icons
         },
-        tag = 'nightly'                    -- optional, updated every week. (see issue #1193)
+        --tag = 'nightly'                    -- optional, updated every week. (see issue #1193)
     }
     use 'nvim-telescope/telescope.nvim'
     use "nvim-telescope/telescope-file-browser.nvim"
